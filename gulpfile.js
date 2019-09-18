84ba835bcdd445e5dab0b3e9cdcc248bbcbc9cb0
Flora René-Louis-Arthur <flora.rene@egmail.com>
2019-09-18 16:12:25 +0200

    Diched injectPartials for mustache to be able to add variables from json

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,55 @@
 var gulp = require("gulp"),
+  sass = require("gulp-sass"),
   browserSync = require("browser-sync").create(),
+  sourcemaps = require("gulp-sourcemaps"),
+  cleanCSS = require("gulp-clean-css"),
+  cache = require("gulp-cache"),
+  mustache = require("gulp-mustache"),
   injectPartials = require("gulp-inject-partials");
 
 gulp.task("reload", function(done) {
+  cache.clearAll();
   browserSync.reload();
   done();
 });
 
 gulp.task("html", function() {
-  return gulp
-    .src("./src/index.html")
-    .pipe(injectPartials())
-    .pipe(gulp.dest("./"));
+  return gulp.src("./src/index.html")
+    .pipe(mustache('data.json',{},{}))
+    .pipe(gulp.dest("./dist"));
+});
+
+gulp.task("sass", function() {
+  return (
+    gulp
+      .src("src/scss/styles.scss")
+      .pipe(sass().on("error", sass.logError))
+      // .pipe(cleanCSS())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest("dist/css/"))
+  );
 });
 
 // Static server
-gulp.task('default', function() {
-  gulp.watch(['src/index.html'], gulp.series('html'));
-  gulp.watch(['src/partials/'], gulp.series('html'));
+gulp.task("default", function() {
+  gulp.watch(
+    ["src/index.html", "src/partials/"],
+    gulp.series(["html"], ["reload"])
+  );
+  gulp.watch(["src/scss/*"], gulp.series(["sass"], ["reload"]));
+  gulp.watch(["dist/js/scripts.js"], gulp.series("reload"));
+
   browserSync.init({
     server: {
-      baseDir: "./"
+      baseDir: "./dist/"
     },
     open: false,
-    files: ['index.html',
-      {match:'src/partials/*.html',
-        fn: gulp.series('html')}
-
+    files: [
+      "index.html",
+      {
+        match: "src/partials/*.html",
+        fn: gulp.series([["reload"]])
+      }
     ]
   });
 });
