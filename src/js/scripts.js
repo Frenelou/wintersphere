227c8d64c1623307a420604f0f0ac42e1ce677e3
Flora René-Louis-Arthur <flora.rene@egmail.com>
2019-09-23 17:36:31 +0200

    responsive video

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,30 +1,64 @@
 $(document).ready(function() {
-  const video1 = document.getElementById("video_1"),
-    video2 = document.getElementById("video_2");
-
+  function loadVideos(id) {
+    if ($(`#${id}`).length < 1) {
+      createNewVideo(id);
+      document.getElementById(`${id}`).play();
+    }
+    else{document.getElementById(`${id}`).play();}
+  }
+  function createNewVideo(id) {
+    $(".video").remove();
+    var video = $("<video />", {
+      id: id,
+      src: $(`#${id}_info`).data("src"),
+      type: "video/mp4",
+      class:"video",
+      width: $(`#${id}_info`).width(),
+      controls: true
+    });
+    video.appendTo(`#${id}_info`);
+  }
+  $(window).on("load resize", function(event) {
+    $(".video_wrapper").each(function(index, el) {
+      createNewVideo($(el).data("id"));
+    });
+    /* Act on the event */
+  });
   // *****************************************************************************
 
   var controller = new ScrollMagic.Controller();
 
   var scene_video1 = new ScrollMagic.Scene({
-    triggerElement: "#video_1"
+    triggerElement: "#video_1_info"
   })
     .on("enter", function() {
-      video1.play();
+      createNewVideo("video_1");
+      loadVideos("video_1");
     })
     .on("leave", function() {
-      video1.pause();
+      $("#video_1").pause();
     })
     .addTo(controller);
-
-  var scene_video2 = new ScrollMagic.Scene({
-    triggerElement: "#video_2"
+  var scene_video1 = new ScrollMagic.Scene({
+    triggerElement: "#video_2_info"
   })
     .on("enter", function() {
-      video2.play();
+      createNewVideo("video_2");
+      loadVideos("video_2");
     })
     .on("leave", function() {
-      video2.pause();
+      $("#video_2").pause();
+    })
+    .addTo(controller);
+  var scene_video1 = new ScrollMagic.Scene({
+    triggerElement: "#video_3_info"
+  })
+    .on("enter", function() {
+      createNewVideo("video_3");
+      loadVideos("video_3");
+    })
+    .on("leave", function() {
+      $("#video_3").pause();
     })
     .addTo(controller);
 });
